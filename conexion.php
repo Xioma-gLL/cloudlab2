cbb74b3e3d9fa80be8dc1303ef510ff97e3c7c02
Keyli Roncal <keyli.roncal@tecsup.edu.pe>
2026-03-25 15:34:39 -0500

    corregui

--- conexion.php
+++ conexion.php
@@ -2,7 +2,7 @@
 
     function conexion(){
 
-    $host = "dpg-d723d88ule4c73bhcut0-a.oregon-postgres.render.com";
+    $host = "host=dpg-d723d88ule4c73bhcut0-a.oregon-postgres.render.com";
     $port = "port=5432";
     $dbname = "dbname=test_d05m";
     $user = "user=test_d05m_user";
